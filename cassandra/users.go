915a4b5228a4ceebfa9aa613a0da30cc2e0588a3
Dejan Mijic <dejan@mainflux.com>
2017-07-23 02:09:49 +0200

    Implement C* user repository
    Signed-off-by: Dejan Mijic <dejan@mainflux.com>

--- cassandra/users.go
+++ cassandra/users.go
@@ -1,6 +1,8 @@
 package cassandra
 
 import (
+	"log"
+
 	"github.com/gocql/gocql"
 	"github.com/mainflux/manager"
 )
@@ -17,9 +19,33 @@ func NewUserRepository(session *gocql.Session) manager.UserRepository {
 }
 
 func (ur *userRepository) Save(user manager.User) error {
-	return manager.ErrConflict
+	cql := `INSERT INTO users (email, password) VALUES (?, ?) IF NOT EXISTS`
+
+	applied, err := ur.session.Query(cql, user.Email, user.Password).ScanCAS()
+	if !applied {
+		return manager.ErrConflict
+	}
+
+	return err
 }
 
 func (ur *userRepository) One(email string) (manager.User, error) {
-	return manager.User{}, manager.ErrNotFound
+	cql := `SELECT email, password FROM users WHERE email = ?`
+
+	user := manager.User{}
+	row := map[string]interface{}{
+		"email":    &user.Email,
+		"password": &user.Password,
+	}
+
+	iter := ur.session.Query(cql, email).Iter()
+	if !iter.MapScan(row) {
+		return user, manager.ErrInvalidCredentials
+	}
+
+	if err := iter.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	return user, nil
 }
